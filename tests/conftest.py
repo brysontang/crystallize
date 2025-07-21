10efa09e7da99ad129280cf40f5e8a2c1add8b2c
Bryson Tang <brysontang@gmail.com>
2025-07-21 17:27:52 -0400

    chore: file networkx test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import types
 # Provide a minimal networkx stub so tests run without the real dependency
 nx = types.ModuleType("networkx")
 
+
 class DiGraph:
     def __init__(self):
         self._succ = {}
@@ -59,8 +60,36 @@ def topological_sort(g: DiGraph):
 
     return order
 
+
+def weakly_connected_components(g: DiGraph):
+    nodes = set(g.nodes.keys())
+    undirected_adj = {n: set() for n in nodes}
+    for u, neighbors in g._succ.items():
+        for v in neighbors:
+            undirected_adj[u].add(v)
+            undirected_adj[v].add(u)
+
+    visited = set()
+    for node in nodes:
+        if node not in visited:
+            component = set()
+            q = [node]
+            visited.add(node)
+            head = 0
+            while head < len(q):
+                curr = q[head]
+                head += 1
+                component.add(curr)
+                for neighbor in undirected_adj.get(curr, []):
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        q.append(neighbor)
+            yield component
+
+
 nx.DiGraph = DiGraph
 nx.is_directed_acyclic_graph = is_directed_acyclic_graph
 nx.topological_sort = topological_sort
+nx.weakly_connected_components = weakly_connected_components
 
 sys.modules.setdefault("networkx", nx)
